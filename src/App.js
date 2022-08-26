99d4ea442ba6a71dfa6f781f29f96f6f7d467c44
Jertt <jeartt.th@gmail.com>
2022-08-26 15:36:51 +0200

    installed the i18next-http-backend package

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import './index.css';
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
-
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
-  .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      },
-      al: {
-        translation: {
-          "Welcome to React": "Mirësevini në React dhe react-i18next"
-        }
-      },
-    },
-    lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
-    detection: {
-      order: ['htmlTag', 'cookie', 'localStorage', 'navigator', 'path', 'subdomain'],
-      caches: ['cookie'],
-    },
-    interpolation: {
-      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    }
-  });
 
 function App() {
-  const { t } = useTranslation();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        {t('Welcome to React')}
-      </h1>
+      
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// imports needed for i18next
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend'
+import App from './App'
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    supportedLngs: ['en', 'al'],
+    fallbackLng: 'en',
+    detection: {
+      order: ['htmlTag', 'cookie', 'localStorage', 'navigator', 'path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react : {
+      useSuspense: false
+    },
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
